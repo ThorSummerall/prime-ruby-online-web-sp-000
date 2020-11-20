92d4fa9190df94b06499a6d5ade5b3082d5404d7
Thor Summerall <thor.summerall@gmail.com>
2020-11-20 18:41:05 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(number)
   # check if it is divisible by one and itself
   if number / 1 && number / number
     return true
-  elsif number.negative?
+  elsif number.negative?()
     return false
   else
     return false
