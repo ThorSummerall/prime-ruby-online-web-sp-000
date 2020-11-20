13f7a02f36a144939383c517d6655a36be86710d
Thor Summerall <thor.summerall@gmail.com>
2020-11-20 18:52:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,11 @@
 def prime?(number)
-  # check if it is divisible by one and itself
-  if number <= 1
-    return false
-  else number / 1 && number / number
-    return true
-  end
+  # first create an array of numbers to divide by
+  divisors = (2...number).to_a
+  # iterate over the array to check if prime
+  divisors.each do |num|
+    if number % num == 0
+      return false
+    else
+      return true
+    end
 end
