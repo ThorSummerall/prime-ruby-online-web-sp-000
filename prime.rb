d4cec7ae9b6603b7ef3645a38a01710caceaa3eb
Thor Summerall <thor.summerall@gmail.com>
2020-11-20 18:57:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,15 @@
 def prime?(number)
-
+  if number == 2 || number < 2 || number % 2 == 0
+    false
+  else
+    testing = 3
+    while testing < number / 2
+      if number % testing == 0
+        return false
+      else
+        testing += 2
+      end
+    end
+    return true
+  end
 end
